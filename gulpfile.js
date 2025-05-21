116a6b51fa91218ade2e21323f52782e6e3791e7
Nastassia Dunaeva <nastassiadunaeva@gmail.com>
2025-05-21 15:27:32 +0300

    add sass to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,17 @@
 import gulp from 'gulp';
 import browserSync from 'browser-sync';
-import cssImport from 'gulp-cssimport'
 import gulpCssimport from 'gulp-cssimport'
 import { deleteAsync } from 'del';
+import sassPkg from 'sass';
+import gulpSass from 'gulp-sass'
+
+const prepros = false;
+
+const sass = gulpSass(sassPkg);
 
 export const html = () => gulp
     .src('src/*.html')
     .pipe(gulp.dest('dist'))
-    .pipe(browserSync.stream());
-
-export const css = () => gulp
-    .src('src/styles/style.css')
-    .pipe(gulpCssimport({
-        extensions: ['css']
-    }))
-    .pipe(gulp.dest('dist/styles'))
     .pipe(browserSync.stream());
 
 export const js = () => gulp
@@ -22,44 +19,61 @@ export const js = () => gulp
     .pipe(gulp.dest('dist/js'))
     .pipe(browserSync.stream());
 
-export const copy = () => gulp
-    .src(['src/fonts/**/*' ,
-        'src/img/**/*'],
-        {
-            base: 'src',
-            encoding: false
+    export const copy = () => gulp
+        .src(['src/fonts/**/*',
+            'src/img/**/*'],
+            {
+                base: 'src',
+                encoding: false
+            }
+        )
+        .pipe(gulp.dest('dist'))
+        .pipe(browserSync.stream({
+            once: true
+        }));
+
+    export const style = () => {
+        if (prepros) {
+            return gulp
+                .src('src/scss/**/*.scss')
+                .pipe(sass().on('error', sass.logError))
+                .pipe(gulp.dest('dist/style'))
+                .pipe(browserSync.stream());
         }
-    )
-    .pipe(gulp.dest('dist'))
-    .pipe(browserSync.stream({
-        once: true
-    }));
+        return gulp
+            .src('src/styles/style.css')
+            .pipe(gulpCssimport({
+                extensions: ['css']
+            }))
+            .pipe(gulp.dest('dist/styles'))
+            .pipe(browserSync.stream());
+    }
 
-export const server = () => {
-    browserSync.init({
-        ui: false,
-        notify: false,
-        // tunnel: true,
-        server: {
-            baseDir: 'dist'
-        }
-    })
+    export const clear = (done) => {
+        deleteSync([path.dist.base], {
+            force: true,
+        });
+        done();
+    };
 
-    gulp.watch('./src/**/*.html', html);
-    gulp.watch('./src/styles/**/*.css', css);
-    gulp.watch('./src/js/**/*.js', js);
-    gulp.watch(['src/fonts/**/*', 'src/img/**/*'], copy)
-};
+    export const server = () => {
+        browserSync.init({
+            ui: false,
+            notify: false,
+            // tunnel: true,
+            server: {
+                baseDir: 'dist'
+            }
+        });
 
-export const clear = (done) => {
-    deleteSync([path.dist.base], {
-        force: true,
-    });
-    done();
-};
+        gulp.watch('./src/**/*.html', html);        
+        gulp.watch('./src/js/**/*.js', js);
+        gulp.watch(['src/fonts/**/*', 'src/img/**/*'], copy);
+        gulp.watch(prepros ? 'src/style/**/*.scss' : './src/styles/**/*.css', style)
+    };
 
-export const base = gulp.parallel(html, css, js, copy);
+    export const base = gulp.parallel(html, style, js, copy);
 
-export const build = gulp.series(clear, base);
+    export const build = gulp.series(clear, base);
 
-export default gulp.series(base, server);
+    export default gulp.series(base, server);
